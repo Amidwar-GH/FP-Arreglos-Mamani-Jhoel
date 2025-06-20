e811493a0c5e071fa0978e8b063a31859aa06d77
Amidwar <amidwar05@gmail.com>
2025-06-20 10:27:52 -0500

    EJERCICIO12-AVANCE: Agregando el ejercicio 12, usando la libreria estandar y la funcion main

---
+++ EJERCICIO12.cpp
@@ -0,0 +1,14 @@
+/*12. Implementar una función que reciba un vector, su longitud y retorne true si el mismo se encuentra ordenado, 
+false en caso contrario. */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+	
+	
+	
+	return 0;
+}
