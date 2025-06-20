1b1099d01454bc3144e4a8d73418677ba257eaf3
Amidwar <amidwar05@gmail.com>
2025-06-19 21:58:05 -0500

    EJERCICIO10-AVANCE: Agregando metodo de la burbuja para ordenar el array

--- EJERCICIO10.cpp
+++ EJERCICIO10.cpp
@@ -7,16 +7,43 @@ tendencia central media, mediana y moda.*/
 using namespace std;
 
 int main(){
+	//sector variables
 	int arreglo[30];
 	int sumatotal,auxmediana,auxmoda,numero;
 	
 	srand(time(0));
 	
+	//sector llenado de array
 	for(int i=0; i<30; i++){
 		arreglo[i]=rand()%901+100;
-		cout<<i+1<<". "<<arreglo[i]<<endl;
+		//cout<<i+1<<". "<<arreglo[i]<<endl;
 	}
 	
+	//sector suma del array
+	int suma=0;
+	for(int i=0; i<30; i++){
+		suma = suma + arreglo[i];
+	}
+	
+	float promedio=suma/30;
+	
+	cout<<"La suma es: "<<suma<<endl;
+	cout<<"\nEl promedio es: "<<promedio;
+	
+	//para el calculo de la mediana antes ordenar el array con el metodo de la burbuja
+	for(int i=0; i < 30-1; i++) {
+    for(int j=0; j < 30-i-1; j++) {
+        if(arreglo[j] > arreglo[j+1]) {
+            // Intercambio de valores
+            int temp= arreglo[j];
+            arreglo[j]= arreglo[j + 1];
+            arreglo[j+1]= temp;
+        }
+    }
+	}
+	for(int i=0; i<30;i++){
+		cout<<arreglo[i]<<" "<<endl;
+	}
 	return 0;
 }
 
