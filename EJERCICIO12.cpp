1319635f5c19d04e5b11058db61b2d2e17189eb1
Amidwar <amidwar05@gmail.com>
2025-06-20 11:11:53 -0500

    EJERCICIO12-ORDEN DE ARREGLO (VERSION FINAL)

--- EJERCICIO12.cpp
+++ EJERCICIO12.cpp
@@ -4,16 +4,34 @@ false en caso contrario. */
 #include<iostream>
 using namespace std;
 
+		//mandamos el array , y su tamanio
+bool ArrOrdenado(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++) {
+        if(arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true; 
+}
+
+
 int main(){
 	int n;
-	int arr[n];
 	bool orden;
 	
 	cout<<"Digite la cantidad de elementos de su arreglo: "; cin>>n;
+	int arr[n];
 		
 	for(int i=0; i<n; i++){
 		cout<<"Digite el elemento "<<i+1<<endl;
 		cin>>arr[i];
+	}
+	
+	orden = ArrOrdenado(arr,n);
+	if(orden){
+		cout<<"El arreglo no se encuentra ordenado."<<endl;
+	}else{
+		cout<<"El arreglo no se encuentra ordenado."<<endl;
 	}
 	
 	return 0;
