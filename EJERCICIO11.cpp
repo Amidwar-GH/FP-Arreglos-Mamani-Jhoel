1af4732c6f6e6f054e177830c15d8ea78f3a0271
Amidwar <amidwar05@gmail.com>
2025-06-20 09:57:35 -0500

    EJERCICIO11-AVANCE: mejorando la logica del problema, logramos sacar la suma general falta el objetivo del enunciado

--- EJERCICIO11.cpp
+++ EJERCICIO11.cpp
@@ -4,16 +4,17 @@
 using namespace std;
 
 int main(){
-	int n;
-	int numeros[n];
+	int n, suma=0;
+	int arr[n];
 	cout<<"¿cuantos numeros desea digitar?"<<endl;
 	cin>>n;
 	
+	cout<<"Digite los elementos del arreglo."<<endl;
 	for(int i=0; i<n; i++){
-		cout<<"Digite el numero "<<i+1<<endl;
-		cin>>numeros[n];
+		cin>>arr[i];
+		suma=suma+arr[i];
 	}
-	
+	cout<<"La suma es "<<suma<<endl;
 	
 	return 0;
 } 
