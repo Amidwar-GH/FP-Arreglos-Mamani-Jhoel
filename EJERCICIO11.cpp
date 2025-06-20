86dbd76a2b44e34366d62bcef6ded1e0da0c12b9
Amidwar <amidwar05@gmail.com>
2025-06-19 23:06:33 -0500

    EJERCICIO11-AVANCE: Agregando el ejercicio 11, agregando librerias y los arrays con bucles for

---
+++ EJERCICIO11.cpp
@@ -0,0 +1,19 @@
+/*11. Calcular la suma de elementos positivos y negativos en un arreglo.*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	int numeros[n];
+	cout<<"¿cuantos numeros desea digitar?"<<endl;
+	cin>>n;
+	
+	for(int i=0; i<n; i++){
+		cout<<"Digite el numero "<<i+1<<endl;
+		cin>>numeros[n];
+	}
+	
+	
+	return 0;
+} 
