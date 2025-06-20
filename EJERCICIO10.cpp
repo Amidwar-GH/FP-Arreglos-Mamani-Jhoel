5c054e37908c11c4430d1ecc141a923dee02653f
Amidwar <amidwar05@gmail.com>
2025-06-19 19:58:59 -0500

    Ejercicio10-avance:agregando el ejercicio, incluyendo libreria e inicializando el generador de numeros aleatorios

---
+++ EJERCICIO10.cpp
@@ -0,0 +1,18 @@
+/*10. Generar 30 números aleatorios entre 100 y 1000 guardarlos en un arreglo y calcular estadísticas de 
+tendencia central media, mediana y moda.*/
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+	int arreglo[30];
+	srand(time(0));
+	rand()%1000+100;
+	
+	
+	
+	return 0;
+}
+
