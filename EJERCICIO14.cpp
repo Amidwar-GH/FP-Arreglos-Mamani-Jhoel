73350dbc7e82348e206b516bedaa3745b1d2430e
Amidwar <amidwar05@gmail.com>
2025-06-21 12:52:25 -0500

    EJERCICIO14-AVANCE: Agregando el ejercicio14, incluyendo las libreria estandar y la funcion main

---
+++ EJERCICIO14.cpp
@@ -0,0 +1,13 @@
+/*14. Generar la función mistrcat(str1,str2) que agrega str2 al final de str1. No usar las funciones de String.h  */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+	
+	
+	
+	return 0;
+}
