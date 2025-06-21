5d0c5cd01149e4c7640eeff27f1fedccecc26e63
Amidwar <amidwar05@gmail.com>
2025-06-20 23:05:22 -0500

    EJERCICIO13-AVANCE: Agregando ejercicio 13, agregando libreria estandar, funcion principal, encabezado de la contruccion void

---
+++ EJERCICIO13.cpp
@@ -0,0 +1,18 @@
+/*13. Generar la función mistrcpy(str1,str2) que copia str2 en str1. No usar la funciones de String.h  */
+
+#include<iostream>
+using namespace std;
+
+//construimos la funcion
+void mistrcpy(char llegada[]; char partida[]);
+
+
+int main(){
+	char str1[100];
+	char str2[100];
+	
+	cout<<"Digite una palabra: "; cin>>str2;
+	//hacemos llamado de la funcion
+	
+	return 0;
+}
