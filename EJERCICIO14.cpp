932ecdb849f7ced7fae2081d90f015855f5be4a2
Amidwar <amidwar05@gmail.com>
2025-06-21 17:13:02 -0500

    EJERCICIO14-AVANCE: Agregando la funcion void para simular la funcion de la libreria string

--- EJERCICIO14.cpp
+++ EJERCICIO14.cpp
@@ -3,10 +3,33 @@
 #include<iostream>
 using namespace std;
 
+void mistrcat(char str1[] , char str2[]){
+	
+	//aca vamos a encontrar el '\0' para saber la posicion final
+	int i=0;
+	while(str1[i] != '\0'){
+		i++;
+	}
+	
+	//ordenando los valores de lo arrays
+	int j=0;
+	while(str2[j] != '\0'){  //<-- aca vamos a recorres hasta que encuentre el nulo 
+		str1[i] = str2[j];   //<-- aca designamos que si poe ejemplo en el anterior el nulo ('\0') acabo en la posicion 3 apartir de ahi vamos a copiar el str2
+		i++;				 //<-- aca vamos a avanzar de posicion en el array [1][1+1=2][2+1=3] por ejemplo y para que con dcada iteracion copiemos el valor de str2 en un casillero en blanco	
+		j++;					//<-- aca sumamos j para que avanze un casilla j
+	}
+	
+	str1[i]= '\0';  //<-- al final del arreglo agregamos el '\0' para indicar el termino del array.
+}
+
 int main(){
+	char str1[100];
+	char str2[100];
 	
-	
-	
+	cout<<"Digite la primera palabra: "; 
+	cin>>str1;
+	cout<<"Digite la segunda palabra: ";
+	cin>>str2;
 	
 	
 	return 0;
