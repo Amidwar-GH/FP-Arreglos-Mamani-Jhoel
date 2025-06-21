66a85269794f0a6d6965a67e8e03c5e023410806
Amidwar <amidwar05@gmail.com>
2025-06-21 17:40:41 -0500

    EJERCICIO15-AVANCE: Agregando ejercicio 15, libreria estandar y funcion main

---
+++ EJERCICIO15.cpp
@@ -0,0 +1,14 @@
+/*15. Generar la función mistrcmp(str1,str2) que compara dos cadenas y retorna los resultados 0 si las cadenas 
+son iguales, 1 si la cadena str1 >str2, -1 si la cadena str2>str1. No usar las funciones de String.h*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+	
+	
+	
+	return 0;
+}
