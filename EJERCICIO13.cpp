522cd8b396953e81cf6f51e9e915ac3033a61c32
Amidwar <amidwar05@gmail.com>
2025-06-21 12:44:35 -0500

    EJERCICIO13-AVANCE: EJERCICIO DE CREACION DE FUNCION mistrcpy (VERSION FINAL)

--- EJERCICIO13.cpp
+++ EJERCICIO13.cpp
@@ -6,11 +6,11 @@ using namespace std;
 //construimos la funcion
 void mistrcpy(char llegada[], char partida[]){
 	int i=0;
-	while(origen[i] != '\0'){
+	while(partida[i] != '\0'){
 		llegada[i] = partida[i];
-		i++
+		i++;
 	}
-	destino[i] = '\0';
+	llegada[i] = '\0';
 }
 
 
@@ -18,8 +18,13 @@ int main(){
 	char str1[100];
 	char str2[100];
 	
-	cout<<"Digite una palabra: "; cin>>str2;
+	cout<<"Digite una palabra para str2: "; 
+	cin.getline(str2,100);
+	
 	//hacemos llamado de la funcion
+	mistrcpy(str1,str2);
+	
+	cout<<"La palabra final copiada de str2 a str1 es: "<<str1<<endl;
 	
 	return 0;
 }
