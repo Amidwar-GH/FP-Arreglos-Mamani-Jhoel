3a4561fccb791dfc4fe0227d814e430ac6308013
Amidwar <amidwar05@gmail.com>
2025-06-21 18:22:29 -0500

    EJERCICIO15-AVANCE: Rellenando los arrays, haciendo llamado de la funcion y hhaciendo la cabecera de la funcion mistrcmp

--- EJERCICIO15.cpp
+++ EJERCICIO15.cpp
@@ -4,11 +4,26 @@ son iguales, 1 si la cadena str1 >str2, -1 si la cadena str2>str1. No usar las f
 #include<iostream>
 using namespace std;
 
+char mistrcmp(char str1[], char str2[]){
+	
+}
+
+
+
 int main(){
+	char comparacion;
+	char str1[100];
+	char str2[100];
 	
+	cout<<"Digite la primera palabra del primer arreglo: ";
+	cin>>str1;
+	cout<<"Digite la segunda palabra del segundo arreglo: ";
+	cin>>str2;
 	
+	cout<<"------------------------------------------------------"<<endl;
+	cout<<"VAMOS A COMPARAR LAS DOS CADENAS...."<<endl;
 	
-	
+	comparacion = mistrcmp(str1, str2); // <-- aca vamos a llamar a la funcion nos va retornar un valor
 	
 	return 0;
 }
