ab1a1b2da8c302eb48a9f7b19fff8bffe1138682
Amidwar <amidwar05@gmail.com>
2025-06-21 20:37:29 -0500

    EJERCICIO16-AVANCE: AGREGANDO EL EJERICIO 16, FUNCION MAIN, DECLARANDO EL ARREGLO Y PIDIENDO LA FRASE

---
+++ EJERCICIO16.cpp
@@ -0,0 +1,16 @@
+/*16. Leer una línea de entrada. Descartar todos los símbolos excepto los dígitos. Convertir la cadena de dígitos en 
+un entero y fijar el valor del entero a la variable n y finalmente sumarle 2 (las funciones atoi y isdigit te pueden 
+ayudar para este propósito) Ejemplo: “En aproximadamente 120 días llegaremos al año 2022 y festejaremos” 
+1202022 -> 1202024*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	char enunciado[150];
+	
+	cout.getline<<"Digite un enunciado: ";
+	cin>>enunciado;
+	
+	return 0;
+}
