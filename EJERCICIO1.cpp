19e7065056103f687873d8792a05acda371170a9
Amidwar <amidwar05@gmail.com>
2025-06-22 20:50:34 -0500

    EJERCICIO1-AVANCE: Agregando algoritmo para recorrer el arreglo, cambiando las primeras letras de cada frase

--- EJERCICIO1.cpp
+++ EJERCICIO1.cpp
@@ -5,11 +5,31 @@ La Humanidad" */
 using namespace std;
 
 int main(){
+	bool RenovacionP;
+	int i = 0;
 	char frase[500];
 	char resultado[500];
 	cout<<"Digite la frase a convertir: ";
 	cin.getline(frase,500);
 	
+	
+	RenovacionP = true;
+	while(frase[i] != '\0'){
+		if(frase[i] == ' '){
+			resultado[i] = ' ';
+			RenovacionP = true; 
+		}else{
+			if(RenovacionP == true){
+				resultado[i] = toupper(frase[i]);
+				RenovacionP = false;
+			}else{
+				resultado[i] = frase[i];
+			}
+		}
+		i=i+1;
+	}
+	
+	resultado[i] = '\0'; 
 	
 	
 	return 0;
