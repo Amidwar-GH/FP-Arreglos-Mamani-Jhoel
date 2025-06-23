14c96375d8688fde47d7b97539ae13fbcfbb3a51
Amidwar <amidwar05@gmail.com>
2025-06-23 01:05:10 -0500

    EJERCICIO7-AVANCE: Definiendo el vector y rellenando con valores

---
+++ EJERCICIO7.cpp
@@ -0,0 +1,20 @@
+/*7. Invertir un vector e imprimirlo. */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	int vector[500];
+	cout<<"Digite el tamanio del vector: "; cin>>n;
+	
+	cout<<"\nDigite los elementos del vector: "<<endl;
+	for(int i=0; i<n; i++){
+		cin>>vector[i];
+	}
+	
+	
+	
+	
+	return 0;
+}
