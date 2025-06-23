3a50bfc774b579ad2556c09a37ea039b2abc24ad
Amidwar <amidwar05@gmail.com>
2025-06-23 10:42:01 -0500

    EJERCICIO18-AVANCE: Añadiendo el ejercicio18, dedclarando variables e implementando en bucle para recorrer el arreglo

---
+++ EJERCICIO18.cpp
@@ -0,0 +1,22 @@
+/*18. Dada una frase comprobar si el balanceo de los paréntesis es correcto. Es decir, “(ccc(ccc)cc((ccc(c))))” es 
+correcto, pero “)ccc(ccc)cc((ccc(c)))(“ no lo es, aunque tenga el mismo número de paréntesis abiertos que 
+cerrados.*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	char texto[500];
+	cout<<"DIGITE EL TEXTO QUE CONTENGA PARENTESIS: ";
+	cin.getline(texto,500);
+	
+	int i=0;
+	while(texto[i]!= '\0'){
+		
+	}
+	
+	
+	
+	return 0;
+}
+
