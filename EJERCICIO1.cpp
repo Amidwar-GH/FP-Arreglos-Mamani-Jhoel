956f57696fd8097c9aa5c0c6941aed407fb70273
Amidwar <amidwar05@gmail.com>
2025-06-22 19:39:38 -0500

    EJERCICIO1-AVANCE: Agregando el ejercicio 1, definiendo variables y peticion de la frase

---
+++ EJERCICIO1.cpp
@@ -0,0 +1,16 @@
+/*1. Convertir palabras a mayúsculas iniciales: "cáceres patrimonio de la humanidad" ? "Cáceres Patrimonio De 
+La Humanidad" */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	char frase[500];
+	char resultado[500];
+	cout<<"Digite la frase a convertir: ";
+	cin.getline(frase,500);
+	
+	
+	
+	return 0;
+}
