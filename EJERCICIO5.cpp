c3a803bb5869e8bbc7552e1dd365e19a1c54c15d
Amidwar <amidwar05@gmail.com>
2025-06-22 23:35:29 -0500

    EJERCICIO5-AVANCE: Encontrando el numero mayor y menor del arreglo

--- EJERCICIO5.cpp
+++ EJERCICIO5.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main(){
+	int numeromayor,numeromenor;
 	int n;
 	int numeros[100];
 	cout<<"Cuantos numeros va digitar?";
@@ -14,6 +15,20 @@ int main(){
 		cout<<"Digite el numero "<<"["<<i+1<<"]"<<endl;
 		cin>>numeros[i];
 	}
+	
+	//vamos a asumir arbitrariamente para la comparacion
+	numeromayor = numeros[0];
+	numeromenor = numeros[0];
+	
+	for(int i=1; i<n; i++){
+		if(numeros[i]>numeromayor){
+			numeromayor=numeros[i];
+		}else if(numeros[i] < numeromenor){
+			numeromenor = numeros[i];
+		}
+		
+	}
+	cout<<numeromayor<<" "<<numeromenor;
 	
 	
 	return 0;
