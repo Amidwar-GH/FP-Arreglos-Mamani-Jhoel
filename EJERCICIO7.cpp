aa8230a1ff06e490522475a900b4431a0346bfec
Amidwar <amidwar05@gmail.com>
2025-06-23 01:20:06 -0500

    EJERCICIO7: Haciendo el intercambio de valores con el metodo de la burbuja (VERSION FINAL)

--- EJERCICIO7.cpp
+++ EJERCICIO7.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-	int n;
+	int n,auxiliar;
 	int vector[500];
 	cout<<"Digite el tamanio del vector: "; cin>>n;
 	
@@ -13,8 +13,23 @@ int main(){
 		cin>>vector[i];
 	}
 	
+	cout<<"Tu vector es: ";
+	for(int i=0; i<n; i++){
+		cout<<vector[i]<<" ";
+	}
 	
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n-1-i; j++){
+			auxiliar = vector[j];
+			vector[j] = vector[j+1];
+			vector[j+1] = auxiliar;
+		}
+	}
 	
+	cout<<"\nTu vector invertido es: "<<endl;
+	for(int i=0; i<n; i++){
+		cout<<vector[i]<<" ";
+	}
 	
 	return 0;
 }
