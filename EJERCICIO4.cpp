c009097058d31b77c558755a7aeb0b647d33fe1c
Amidwar <amidwar05@gmail.com>
2025-06-22 21:40:47 -0500

    EJERCICIO4-AVANCE: Agregando el ejercicio 4 duncion main, declarando variables

---
+++ EJERCICIO4.cpp
@@ -0,0 +1,12 @@
+/*4. Leer una secuencia de nombres, ordenarlos alfabéticamente y presentarlos. */
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Cuantos nombres va digitar?: "; cin>>n;
+	cin>>n;
+	char nombres[100][30]; //<-- 100 nombres de longitud 30
+	
+	return 0;
+}
