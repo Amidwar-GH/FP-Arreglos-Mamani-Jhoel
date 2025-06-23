1b4063e3eda61b9a0cb396ecfbbbd76de1625bb4
Amidwar <amidwar05@gmail.com>
2025-06-22 19:17:15 -0500

    EJERCICIO17: COMPROBACION DE APELLIDO (VERSION FINAL)

--- EJERCICIO17.cpp
+++ EJERCICIO17.cpp
@@ -2,21 +2,37 @@
 pertenece a esa persona.*/
 
 #include<iostream>
+#include<string.h>
 using namespace std;
+
+//construccion de la funcion
+
+bool funcionApellido(char nombresCompletos[], char apellido[]){
+	
+	//buscamos el apellido en el areglo del nombre					//(strstr(nombresCompletos, apellido) 
+	if(strstr(nombresCompletos, apellido) != NULL){   	//<BUSCAMOS SI APELLIDOS ESTA DENTRO DE NOMBRESCOMPLETOS>
+		return true;
+	}else{
+		return false;
+	}
+}
+
 
 int main(){
 	bool veredicto;
 	char nombresCompletos[300];
 	char apellido[300];
 	
+	//llenado del arreglo
 	cout<<"Digite su nombre completo y el apellido: ";
 	cin.getline(nombresCompletos,300);
 	cout<<"Digite el apellido a buscar: ";
 	cin.getline(apellido,300);
 	
+	//llamado de la funcion
 	veredicto = funcionApellido(nombresCompletos, apellido);
 	
-	if(Veredicto == true){
+	if(veredicto == true){
 		cout<<"El apellido si pertenece a esa persona.";
 	}else{
 		cout<<"El apellido no se ha encontrado.";
